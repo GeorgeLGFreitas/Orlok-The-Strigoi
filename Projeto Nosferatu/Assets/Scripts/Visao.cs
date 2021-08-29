f06c2f7d3202a72d0de97c623e5898f5e412cacb
GeorgeLGFreitas <georgelgfreitas@gmail.com>
2021-08-29 18:48:17 -0300

    Added Vision Script

---
+++ Projeto Nosferatu/Assets/Scripts/Visao.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Visao : MonoBehaviour
+{
+    
+  public float sensibility = 0f;
+  Transform braco;
+  Vector3 bracoRotation = Vector3.zero;
+  
+    void Start()
+    {
+    
+        braco = transform.Find("Braco");
+        Cursor.lockState = CursorLockMode.Locked;   
+          
+    }
+
+    
+    void Update()
+    {
+        Vector3 rotation = transform.localEulerAngles;
+        rotation.y += Input.GetAxis("Mouse X") * sensibility;
+        transform.localEulerAngles = rotation;
+    
+        bracoRotation.x -= Input.GetAxis("Mouse Y") * sensibility;
+        bracoRotation.x = Mathf.Clamp(bracoRotation.x, -60, 60);
+        braco.localEulerAngles = bracoRotation;
+        
+    }
+}
