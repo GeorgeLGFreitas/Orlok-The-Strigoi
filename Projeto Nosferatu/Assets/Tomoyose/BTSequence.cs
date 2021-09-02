2f155a40d702d052a4efe56852f74a45eff02af7
GeorgeLGFreitas <georgelgfreitas@gmail.com>
2021-09-02 16:53:14 -0300

    Added BTSequence Script

---
+++ Projeto Nosferatu/Assets/Tomoyose/BTSequence.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public class BTSequence : BTNode
+{
+    List<BTNode> children = new List<BTNode>();
+
+    public override IEnumerator Run(BehaviourTree bt)
+    {
+        status = Status.RUNNING;
+
+        foreach (BTNode node in children)
+        {
+            yield return bt.StartCoroutine(node.Run(bt));
+            if (node.status == Status.FAILURE)
+            {
+                status = Status.FAILURE;
+                break;
+            }
+            
+        }
+
+        if (status == Status.RUNNING)
+        {
+            status = Status.SUCCESS;
+        }
+    }
+}
