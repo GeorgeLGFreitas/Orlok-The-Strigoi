967b4e8d025d348cc99080ed26fa3ed12ea05d92
GeorgeLGFreitas <georgelgfreitas@gmail.com>
2021-11-08 16:32:03 -0300

    Added BTTemBarulho Script

---
+++ Projeto Nosferatu/Assets/Scripts/BehaviourTreeNosferatu/BTTemBarulho.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BTTemBarulho : BTNode
+{
+    public override IEnumerator Run(BehaviourTreeNosferatu bt)
+    {
+        status = Status.FAILURE;
+        GameObject alvo = GameObject.FindGameObjectWithTag("Atencao");
+        
+        if (Vector3.Distance(bt.transform.position, alvo.transform.position) < 20)
+        {
+            status = Status.SUCCESS;
+            
+        }
+        
+        Print(bt);
+        yield break;
+    }
+}
