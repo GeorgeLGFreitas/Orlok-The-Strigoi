7c3fdb4cdf584040c0f33978e26681664030de30
GeorgeLGFreitas <georgelgfreitas@gmail.com>
2021-11-23 13:33:22 -0300

    Changes to George scene

--- Projeto Nosferatu/Assets/Scripts/BehaviourTreeNosferatu/BehaviourTreeNosferatu.cs
+++ Projeto Nosferatu/Assets/Scripts/BehaviourTreeNosferatu/BehaviourTreeNosferatu.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BehaviourTreeNosferatu : MonoBehaviour
 {
     public BTNode root;
+    public GameObject player;
 
+    public NavMeshAgent agente;
+    
     public IEnumerator Begin()
     {
         while(true)
         {
             yield return StartCoroutine(root.Run(this));
         }
+    }
+
+    void Start()
+    {
+        agente = GetComponent<NavMeshAgent>();
     }
 }
